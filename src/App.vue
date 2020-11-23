6f53fdcb1ce81963c470a2f620ef8bc9417f10c0
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-11-23 21:20:25 +0100

    First commit; tau-prolog PoC

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+
+    <p>Kolor</p>
+    <input type="radio" id="c1" value="zolty" v-model="color">
+    <label for="n1">Żółty</label>
+    <input type="radio" id="c2" value="zielony" v-model="color">
+    <label for="n2">Zielony</label>
+    <input type="radio" id="c3" value="czerwony" v-model="color">
+    <label for="n2">Czerwony</label>
+    <input type="radio" id="c4" value="czarny" v-model="color">
+    <label for="n2">Czarny</label>
+<!--    <p>{{color}}</p>-->
+
+    <p>Numer</p>
+    <input type="radio" id="n1" value="parzysty" v-model="number">
+    <label for="n1">Parzysty</label>
+    <input type="radio" id="n2" value="nieparzysty" v-model="number">
+    <label for="n2">Nieparzysty</label>
+<!--    <p>{{number}}</p>-->
+    <p/>
+
+    <button @click="run">Run it</button>
+    <p v-for="(result, index) in results" :key="index">{{result}}</p>
+  </div>
+</template>
+
+<script>
+// import pl from 'tau-prolog'
+import { getAnswersProvider } from './backend'
+
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      color: null,
+      number: null,
+      results: []
+    }
+  },
+  computed: {
+    facts () {
+
+      let facts = []
+      if (this.number) {
+        facts.push({
+          key: 'numer',
+          value: this.number
+        })
+      }
+      if (this.color) {
+        facts.push({
+          key: 'kolor',
+          value: this.color
+        })
+      }
+      return facts
+    }
+  },
+  methods: {
+    run() {
+      this.results = []
+      getAnswersProvider(this.facts).then(session => {
+        session.answers(answer => {
+          // console.log(answer)
+          // console.log(pl.format_answer(answer))
+
+          if (answer === false) {
+            return
+          }
+          this.results.push(answer.links["X"].id)
+        })
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/backend.js
@@ -0,0 +1,101 @@
+import pl from 'tau-prolog'
+
+const database = [
+    "dom(pierwszy, kolor(zielony), numer(nieparzysty)).",
+    "dom(drugi, kolor(zolty), numer(parzysty)).",
+    "dom(czwarty, kolor(czerwony), numer(parzysty)).",
+    "dom(kwantowy, kolor(zielony), numer([nieparzysty, parzysty]))."
+]
+
+const rules = [
+    "znajdz(X) :- dom(X, kolor(Kolor), numer(Numer) ), testuj(Kolor, Numer).",
+
+    "testuj(Kolor, Numer) :- czy_kolor(Kolor), czy_numer(Numer), !.",
+    "testuj(Kolor, _) :- czy_kolor(Kolor).",
+
+    "czy_kolor(X) :- czy_prawda(kolor, X).",
+    "czy_numer(X) :- czy_prawda(numer, X).",
+
+    ":- dynamic([znane/2]).",
+
+    "czy_prawda(X, [Y | _]) :- czy_prawda(X, Y), !.",
+    "czy_prawda(X, [_ | Ys]) :- czy_prawda(X, Ys), !.",
+    "czy_prawda(_, []) :- false.",
+    "czy_prawda(X, Y) :- znane(X, Y), !."
+]
+
+const findRule = "znajdz(X)"
+
+
+function getAnswers() {
+    return []
+}
+
+function getAnswersProvider(facts) {
+    return new Promise((resolve, reject) => {
+        const query = getGoalQuery(facts)
+        console.log(query)
+
+        getQueryProvider(query).then(session => {
+            resolve(session)
+        }).catch(reason => {
+            reject(reason)
+        })
+    })
+}
+
+// function answerReader(session) {
+//     session.answer({
+//         success: function(answer) { /* Answer */ },
+//         error:   function(err) { /* Uncaught error */ },
+//         fail:    function() { /* Fail */ },
+//         limit:   function() { /* Limit exceeded */ }
+//     })
+// }
+
+function getGoalQuery(facts) {
+    return [
+        ...facts.map((fact) => getAssertRule(fact)),
+        findRule
+    ].join(", ") + "."
+}
+
+function getAssertRule({key, value}) {
+    return `assertz(znane(${key}, ${value}))`
+}
+
+
+function getQueryProvider(query) {
+    return new Promise((resolve, reject) => {
+        getSessionProvider().then(session => {
+            session.query(query, {
+                success: () => resolve(session),
+                error: (err) => reject(err)
+            })
+        }).catch(reason => {
+            reject(reason)
+        })
+    })
+}
+
+
+function getSessionProvider() {
+    return new Promise((resolve, reject) => {
+        const program = getProgram()
+        const session = pl.create()
+        session.consult( program, {
+            success: () => resolve(session),
+            error: (err) => reject(err)
+        } )
+    })
+}
+
+function getProgram() {
+    return [
+        ...database,
+        ...rules
+    ].join(" ")
+}
+
+
+export { getAnswersProvider, getAnswers }
