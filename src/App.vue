09f79999b0c005e9f5ccfe9d63313adce31177ae
Ludwik Ciechański <ludwik.ciechanski@futurum.tech>
2020-11-29 00:14:28 +0100

    Implement beta version with dendrology database

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,141 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Section</h1>
+      <h1 class="title">Drzewiasty system ekspertowy</h1>
       <h2 class="subtitle">
-        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+        Wybierz proszę cechy charakterystyczne Twojego drzewa, a ja podam Ci jego gatunek.
       </h2>
 
       <div class="content"> <!--box-->
         <form>
-          <b-field label="Kolor">
-            <b-radio v-model="color"
-                     name="color"
-                     native-value="zolty">
-              Żółty
-            </b-radio>
-            <b-radio v-model="color"
-                     name="color"
-                     native-value="zielony">
-              Zielony
-            </b-radio>
-            <b-radio v-model="color"
-                     name="color"
-                     native-value="czerwony">
-              Czerwony
-            </b-radio>
-            <b-radio v-model="color"
-                     name="color"
-                     native-value="czarny">
-              Czarny
-            </b-radio>
+          <b-field label="Typ">
+            <b-radio-button v-model="chosen.typ" v-for="item in typ" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
           </b-field>
-          <!--    <p class="content">-->
-          <!--      <b>Selection:</b>-->
-          <!--      {{ color }}-->
-          <!--    </p>-->
 
-          <b-field label="Numer">
-            <b-radio v-model="number"
-                     name="number"
-                     native-value="parzysty">
-              Parzysty
-            </b-radio>
-            <b-radio v-model="number"
-                     name="number"
-                     native-value="nieparzysty">
-              Nieparzysty
-            </b-radio>
+          <b-field label="Zimozielone">
+            <b-radio-button v-model="chosen.zimozielone" v-for="item in zimozielone" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
           </b-field>
-          <!--    <p class="content">-->
-          <!--      <b>Selection:</b>-->
-          <!--      {{ number }}-->
-          <!--    </p>-->
 
-            <b-button
-                type="is-primary"
-                @click="run">
-              Run it
-            </b-button>
+          <b-field label="Kora">
+            <b-radio-button v-model="chosen.kora" v-for="item in kora" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
+          </b-field>
+
+          <b-field label="Pokrój">
+            <b-radio-button v-model="chosen.pokroj" v-for="item in pokroj" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
+          </b-field>
+
+          <b-field label="Korona">
+            <b-radio-button v-model="chosen.korona" v-for="item in korona" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
+          </b-field>
+
+          <b-field label="Owoc">
+            <b-radio-button v-model="chosen.owoc" v-for="item in owoc" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
+          </b-field>
+
+          <template v-if="chosen.typ === 'lisciaste'">
+            <b-field label="Kształt liścia">
+              <b-radio-button v-model="chosen.ksztalt_liscia" v-for="item in ksztalt_liscia" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Krawędź liścia">
+              <b-radio-button v-model="chosen.krawedz_liscia" v-for="item in krawedz_liscia" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Położenie liścia">
+              <b-radio-button v-model="chosen.polozenie_liscia" v-for="item in polozenie_liscia" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Krotność liścia">
+              <b-radio-button v-model="chosen.krotnosc_liscia" v-for="item in krotnosc_liscia" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+          </template>
+
+          <template v-if="chosen.typ === 'iglaste'">
+            <b-field label="Długość igły">
+              <b-radio-button v-model="chosen.dlugosc_igly" v-for="item in dlugosc_igly" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Miękkość igły">
+              <b-radio-button v-model="chosen.miekkosc_igly" v-for="item in miekkosc_igly" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Kolor igły">
+              <b-radio-button v-model="chosen.kolor_igly" v-for="item in kolor_igly" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Kształt szyszki">
+              <b-radio-button v-model="chosen.ksztalt_szyszki" v-for="item in ksztalt_szyszki" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+            <b-field label="Wektor szyszki">
+              <b-radio-button v-model="chosen.wektor_szyszki" v-for="item in wektor_szyszki" :key="item" :native-value="item">
+                {{ item }}
+              </b-radio-button>
+            </b-field>
+
+          </template>
+
+          <!-- <b-field v-for="(property, value) in chosen" :label="property" :key="value">
+            <b-radio-button v-model="this.property" v-for="item in this.property" :key="item" :native-value="item">
+              {{ item }}
+            </b-radio-button>
+          </b-field> -->
+
+          <b-button type="is-primary" @click="run" class="mr-3">
+            Uruchom
+          </b-button>
+          
+          <b-button type="is-primary" @click="clear">
+            Nowe zapytanie
+          </b-button>
         </form>
       </div>
 
-      <h1 class="title">Results</h1>
+      <h1 class="title">Wyniki</h1>
       <div
           class="box"
           v-for="(result, index) in results"
           :key="index">
         <article class="media">
           <div class="media-left">
-            <figure class="image is-128x128">
-              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+            <figure class="image">
+              <img :src="getImageURL(result)" v-bind:alt="result">
             </figure>
           </div>
           <div class="media-content">
             <div class="content">
-              <p>
+              <h2>
                 <strong>{{result}}</strong>
-                <br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
+              </h2>
+              [Opis drzewa]
             </div>
           </div>
         </article>
@@ -94,27 +152,69 @@ export default {
   name: 'App',
   data: () => {
     return {
-      color: null,
-      number: null,
-      results: []
+      typ: ['lisciaste', 'iglaste'],
+      zimozielone: ['tak', 'nie'],
+      kora: ['bura', 'brunatna', 'jasna', 'szara', 'popielata', 'brazowa', 'biala'],
+      pokroj: ['okraglawy', 'kolumnowy', 'stozkowaty'],
+      korona: ['rozlozysta', 'kopulasta', 'luzna', 'gesta', 'szeroka', 'stozkowata'],
+      owoc: ['zoladz', 'orzech', 'kulka', 'bukiew', 'kasztan', 'szyszka'],
+      ksztalt_liscia: ['klapiste', 'odwrocone_serca', 'sercowate', 'podluzne', 'jajowate', 'eliptyczne', 'okraglawe', 'palczaste'],
+      krawedz_liscia: ['gladka', 'pilkowana', 'karbowana', 'zabkowana'],
+      polozenie_liscia: ['skretolegle', 'naprzemianlegle'],
+      krotnosc_liscia: ['pojedyncze', 'pierzasto_zlozone'],
+      dlugosc_igly: ['dlugie', 'krotkie'],
+      miekkosc_igly: ['klujace', 'miekkie'],
+      kolor_igly: ['niebiesko_zielone', 'zielony_z_2_paskami', 'zielone'],
+      ksztalt_szyszki: ['okraglawy', 'podluzny'],
+      wektor_szyszki: ['wzniesiona', 'zwisajaca', '?'],
+
+      chosen: {
+        typ: null,
+        zimozielone: null,
+        kora: null,
+        pokroj: null,
+        korona: null,
+        owoc: null,
+        ksztalt_liscia: null,
+        krawedz_liscia: null,
+        polozenie_liscia: null,
+        krotnosc_liscia: null,
+        dlugosc_igly: null,
+        miekkosc_igly: null,
+        kolor_igly: null,
+        ksztalt_szyszki: null,
+        wektor_szyszki: null
+      },
+      results: [],
+
+      images: {
+        dab: "http://ekoexpo.pl/files/obrazki%20artykuly/quercus-robur003.jpeg",
+        lipa: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Tilia_cordata_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-139.jpg",
+        jarzab: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Illustration_Sorbus_aucuparia0.jpg/800px-Illustration_Sorbus_aucuparia0.jpg",
+        olcha: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Illustration_Alnus_glutinosa0.jpg/800px-Illustration_Alnus_glutinosa0.jpg",
+        buk: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Illustration_Fagus_sylvatica0.jpg/800px-Illustration_Fagus_sylvatica0.jpg",
+        grab: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Illustration_Carpinus_betulus_1.jpg/800px-Illustration_Carpinus_betulus_1.jpg",
+        brzoza: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Illustration_Betula_pendula0.jpg/800px-Illustration_Betula_pendula0.jpg",
+        kasztanowiec: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Illustration_Aesculus_hippocastanum0.jpg/371px-Illustration_Aesculus_hippocastanum0.jpg",
+        sosna: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Illustration_Pinus_sylvestris0.jpg/800px-Illustration_Pinus_sylvestris0.jpg",
+        jodla: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Illustration_Abies_alba0.jpg/392px-Illustration_Abies_alba0.jpg",
+        swierk: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Illustration_Picea_abies0_clean.jpg/447px-Illustration_Picea_abies0_clean.jpg",
+        modrzew: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Illustration_Larix_decudua0.jpg/448px-Illustration_Larix_decudua0.jpg"
+      }
     }
   },
   computed: {
     facts () {
-
       let facts = []
-      if (this.number) {
-        facts.push({
-          key: 'numer',
-          value: this.number
-        })
+      for (const [key, value] of Object.entries(this.chosen)) {
+        if (value !== null) {
+          facts.push({
+            key: key,
+            value: value
+          })
+        }
       }
-      if (this.color) {
-        facts.push({
-          key: 'kolor',
-          value: this.color
-        })
-      }
+      // console.log(facts)
       return facts
     }
   },
@@ -125,8 +225,14 @@ export default {
       }).catch(err => {
         console.log(err)
       })
+    },
+    clear() {
+      Object.keys(this.chosen).forEach(k => this.chosen[k] = null)
+      this.results = []
+    },
+    getImageURL(result) {
+      return this.images[result]
     }
   }
 }
 </script>
-
--- src/backend.js
+++ src/backend.js
@@ -1,23 +1,64 @@
 import pl from 'tau-prolog'
 
 const database = [
-    "dom(pierwszy, kolor(zielony), numer(nieparzysty)).",
-    "dom(drugi, kolor(zolty), numer(parzysty)).",
-    "dom(czwarty, kolor(czerwony), numer(parzysty)).",
-    "dom(kwantowy, kolor(zielony), numer([nieparzysty, parzysty]))."
+    "drzewo(dab,zimozielone(nie),kora(bura),pokroj(okraglawy),korona([rozlozysta, kopulasta]),owoc(zoladz),lisciaste(liscie(ksztalt([klapiste, odwrocone_serca]),krawedz(gladka),polozenie(skretolegle),krotnosc(pojedyncze)))).",
+    "drzewo(lipa,zimozielone(nie),kora(brunatna),pokroj(okraglawy),korona(rozlozysta),owoc(orzech),lisciaste(liscie(ksztalt(sercowate),krawedz(pilkowana),polozenie(skretolegle),krotnosc(pojedyncze)))).",
+    "drzewo(jarzab,zimozielone(nie),kora(jasna),pokroj(okraglawy),korona(luzna),owoc(kulka),lisciaste(liscie(ksztalt(podluzne),krawedz(pilkowana),polozenie(skretolegle),krotnosc(pierzasto_zlozone)))).",
+    "drzewo(olcha,zimozielone(nie),kora(szara),pokroj(okraglawy),korona(kopulasta),owoc(orzech),lisciaste(liscie(ksztalt(jajowate),krawedz(pilkowana),polozenie(naprzemianlegle),krotnosc(pojedyncze)))).",
+    "drzewo(buk,zimozielone(nie),kora([popielata, szara]),pokroj([okraglawy, kolumnowy]),korona([gesta, szeroka]),owoc(bukiew),lisciaste(liscie(ksztalt(eliptyczne),krawedz([karbowana, zabkowana]),polozenie(naprzemianlegle),krotnosc(pojedyncze)))).",
+    "drzewo(grab,zimozielone(nie),kora(popielata),pokroj(okraglawy),korona(rozlozysta),owoc(orzech),lisciaste(liscie(ksztalt([eliptyczne, jajowate]),krawedz(pilkowana),polozenie(skretolegle),krotnosc(pojedyncze)))).",
+    "drzewo(brzoza,zimozielone(nie),kora(biala),pokroj(kolumnowy),korona(luzna),owoc(orzech),lisciaste(liscie(ksztalt([okraglawe, jajowate]),krawedz(pilkowana),polozenie(skretolegle),krotnosc(pojedyncze)))).",
+    "drzewo(kasztanowiec,zimozielone(nie),kora([olowianoszara, ciemnobrazowa]),pokroj(okraglawy),korona(kopulasta),owoc(kasztan),lisciaste(liscie(ksztalt(palczaste),krawedz(pilkowana),polozenie(skretolegle),krotnosc(pojedyncze)))).",
+
+    "drzewo(sosna,zimozielone(tak),kora([szara, brazowa]),pokroj(stozkowaty),korona(luzna),owoc(szyszka),iglaste(igly(dlugosc(dlugie),miekkosc(klujace),kolor(niebiesko_zielone)),szyszka(ksztalt(okraglawy),wektor(?)))).",
+    "drzewo(jodla,zimozielone(tak),kora([popielata, szara]),pokroj(stozkowaty),korona(stozkowata),owoc(szyszka),iglaste(igly(dlugosc(krotkie),miekkosc(miekkie),kolor(zielony_z_2_paskami)),szyszka(ksztalt(podluzny),wektor(wzniesiona)))).",
+    "drzewo(swierk,zimozielone(tak),kora(bura),pokroj(stozkowaty),korona(stozkowata),owoc(szyszka),iglaste(igly(dlugosc(krotkie),miekkosc(klujace),kolor(zielone)),szyszka(ksztalt(podluzny),wektor(zwisajaca)))).",
+    "drzewo(modrzew,zimozielone(nie),kora(brazowa),pokroj(stozkowaty),korona([stozkowata, luzna]),owoc(szyszka),iglaste(igly(dlugosc(krotkie),miekkosc(klujace),kolor(zielone)),szyszka(ksztalt(okraglawy),wektor(?))))."
 ]
 
 const rules = [
-    "znajdz(X) :- dom(X, kolor(Kolor), numer(Numer) ), testuj(Kolor, Numer).",
+    "znajdz(X) :- lisciaste(X), !.",  
+    "znajdz(X) :- iglaste(X).",
+    
+    "lisciaste(X) :- testuj_typ(lisciaste),drzewo(X,_,_,_,_,_,lisciaste(liscie(ksztalt(Ksztalt),krawedz(Krawedz),polozenie(Polozenie),krotnosc(Krotnosc)))),testuj_lisciaste(X, Ksztalt, Krawedz, Polozenie, Krotnosc).",
+    "testuj_lisciaste(X, Ksztalt, Krawedz, Polozenie, Krotnosc) :- czy_liscie(Ksztalt, Krawedz, Polozenie, Krotnosc), cechy_wspolne(X), !.",
+    "testuj_lisciaste(_, Ksztalt, Krawedz, Polozenie, Krotnosc) :- czy_liscie(Ksztalt, Krawedz, Polozenie, Krotnosc).",
 
-    "testuj(Kolor, Numer) :- czy_kolor(Kolor), czy_numer(Numer), !.",
-    "testuj(Kolor, _) :- czy_kolor(Kolor).",
+    "iglaste(X) :-testuj_typ(iglaste),drzewo(X,_,_,_,_,_,iglaste(igly(dlugosc(Dlugosc),miekkosc(Miekkosc),kolor(Kolor)),szyszka(ksztalt(Ksztalt),wektor(Wektor)))),testuj_iglaste(X, Dlugosc, Miekkosc, Kolor, Ksztalt, Wektor).",
+    "testuj_iglaste(X, Dlugosc, Miekkosc, Kolor, Ksztalt, Wektor) :- czy_igly(Dlugosc, Miekkosc, Kolor), czy_szyszka(Ksztalt, Wektor),cechy_wspolne(X), !.",
+    "testuj_iglaste(_, Dlugosc, Miekkosc, Kolor, Ksztalt, Wektor) :- czy_igly(Dlugosc, Miekkosc, Kolor), czy_szyszka(Ksztalt, Wektor), !.",
+    "testuj_iglaste(_, Dlugosc, Miekkosc, Kolor, _, _) :- czy_igly(Dlugosc, Miekkosc, Kolor), !.",
+    "testuj_iglaste(_, _, _, _, Ksztalt, Wektor) :- czy_szyszka(Ksztalt, Wektor).",
 
-    "czy_kolor(X) :- czy_prawda(kolor, X).",
-    "czy_numer(X) :- czy_prawda(numer, X).",
+    "testuj_typ(Typ) :- czy_prawda(typ, Typ).",
+
+    "cechy_wspolne(X) :-drzewo(X,zimozielone(Zimozielone),kora(Kora),pokroj(Pokroj),korona(Korona),owoc(Owoc),_),",
+    "testuj_cechy_wspolne(Zimozielone, Kora, Pokroj, Korona, Owoc).",
+    "testuj_cechy_wspolne(Zimozielone, Kora, Pokroj, Korona, Owoc) :- testuj_kpk(Kora, Pokroj, Korona), czy_zimozielone(Zimozielone), czy_owoc(Owoc), !.",
+    "testuj_cechy_wspolne(Zimozielone, _, _, _, Owoc) :- czy_zimozielone(Zimozielone), czy_owoc(Owoc).",
+
+    "testuj_kpk(Kora, Pokroj, Korona) :- czy_kora(Kora),czy_pokroj(Pokroj),czy_korona(Korona), !.",
+    "testuj_kpk(_, Pokroj, Korona) :- czy_pokroj(Pokroj), czy_korona(Korona).",
+
+    "czy_liscie(Ksztalt, Krawedz, Polozenie, Krotnosc) :- czy_prawda(ksztalt_liscia, Ksztalt), czy_prawda(krawedz_liscia, Krawedz), czy_prawda(polozenie_liscia, Polozenie), czy_prawda(krotnosc_liscia, Krotnosc), !.", 
+    "czy_liscie(Ksztalt, Krawedz, _, _) :- czy_prawda(ksztalt_liscia, Ksztalt), czy_prawda(krawedz_liscia, Krawedz), !.",
+    "czy_liscie(Ksztalt, _, _, _) :- czy_prawda(ksztalt_liscia, Ksztalt).",
+
+    "czy_igly(Dlugosc, Miekkosc, Kolor) :- czy_prawda(miekkosc_igly, Miekkosc), czy_prawda(kolor_igly, Kolor), czy_prawda(dlugosc_igly, Dlugosc), !.",
+    "czy_igly(Dlugosc, _, Kolor) :- czy_prawda(kolor_igly, Kolor), czy_prawda(dlugosc_igly, Dlugosc), !.",
+    "czy_igly(Dlugosc, _, _) :- czy_prawda(dlugosc_igly, Dlugosc).",
+
+    "czy_szyszka(Ksztalt, Wektor) :- czy_prawda(ksztalt_szyszki, Ksztalt), czy_prawda(wektor_szyszki, Wektor), !.",
+    "czy_szyszka(Ksztalt, _) :- czy_prawda(ksztalt_szyszki, Ksztalt), !.",
+    "czy_szyszka(_, Wektor) :- czy_prawda(wektor_szyszki, Wektor).",
+
+    "czy_zimozielone(X) :- czy_prawda(zimozielone, X).",
+    "czy_kora(Kora) :- czy_prawda(kora, Kora).",
+    "czy_pokroj(Pokroj) :- czy_prawda(pokroj, Pokroj).",  
+    "czy_korona(Korona) :- czy_prawda(korona, Korona).",
+    "czy_owoc(Owoc) :- czy_prawda(owoc, Owoc).",
 
     ":- dynamic([znane/2]).",
-
     "czy_prawda(X, [Y | _]) :- czy_prawda(X, Y), !.",
     "czy_prawda(X, [_ | Ys]) :- czy_prawda(X, Ys), !.",
     "czy_prawda(_, []) :- false.",
@@ -30,6 +71,7 @@ const findRule = "znajdz(X)"
 function getAnswersProvider(facts) {
     return new Promise((resolve, reject) => {
         const query = getGoalQuery(facts)
+        console.log(query)
 
         getQueryProvider(query).then(session => {
             let answers = []
