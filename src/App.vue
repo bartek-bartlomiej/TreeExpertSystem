d7b1bcf5a7a892e2d0a22b745f8c206e9720cb79
Ludwik Ciechański <ludwik.ciechanski@futurum.tech>
2020-12-04 20:36:33 +0100

    Add short descriptions for trees database

--- src/App.vue
+++ src/App.vue
@@ -340,62 +340,62 @@ export default {
       treeData: {
         dab: {
           displayName: "Dąb",
-          description: "",
+          description: "Jest gatunkiem długowiecznym, żyje ponad 700 lat. Jest powszechnie uważany za jedno z najszlachetniejszych drzew i praktycznie wszędzie na terenie swojego występowania cieszy się wyjątkowym szacunkiem i sympatią. Ze względu na okazałe rozmiary jakie osiąga, sprawia majestatyczne wrażenie.",
           image: "http://ekoexpo.pl/files/obrazki%20artykuly/quercus-robur003.jpeg",
         },
         lipa: {
           displayName: "Lipa",
-          description: "",
+          description: "Wysokie i bardzo okazałe drzewo liściaste o grubym pniu oraz gęstej, szerokiej koronie. Jest często sadzona w parkach, wzdłuż dróg, na osiedlach i przy kościołach. Lubi świeże i żyzne, piaszczysto-ilaste gleby, dobrze znosi zacienienie, jest najbardziej mrozoodporna ze wszystkich lip, ale też jak większość z nich atakowana przez liczne szkodniki i mało odporna na zanieczyszczenia powietrza.",
           image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Tilia_cordata_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-139.jpg",
         },
         jarzab: {
           displayName: "Jarząb",
-          description: "",
+          description: "Niewielkie drzewko o dość regularnym, ładnym pokroju i ciekawych liściach. Jego największą dekoracją są owoce – drobne kuleczki, które naniziane na nitkę wyglądają jak najpiękniejsze korale - jarzębina.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Illustration_Sorbus_aucuparia0.jpg/800px-Illustration_Sorbus_aucuparia0.jpg",
         },
         olcha: {
           displayName: "Olcha",
-          description: "",
+          description: "Olsza jest drzewem liściastym. Jest gatunkiem szybkorosnącym, żyje do ok. 120 lat. Najbardziej charakterystyczną cechą olszy czarnej, są niewielkie, szyszeczkowate owocostany osadzone na trzoneczkach i utrzymujące się na drzewie przez kilka lat.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Illustration_Alnus_glutinosa0.jpg/800px-Illustration_Alnus_glutinosa0.jpg",
         },
         buk: {
           displayName: "Buk",
-          description: "",
+          description: "Okazałe, potężnie zbudowane drzewo liściaste o grubym pniu, masywnych konarach i szerokiej, kopulasto wysklepionej koronie. Osiąga wiek do 350 lat. Najbardziej charakterystyczną cechą buka, pozwalającą bez trudu rozpoznać to drzewo o każdej porze roku, jest jego jasna, szara, zawsze gładka kora. Lubi dość wysoką wilgotność powietrza i jest wrażliwy na wiosenne przymrozki, dlatego brak jest jego naturalnych stanowisk w centralnej i wschodniej Polsce. Rośnie na glebach żyznych, wapiennych, napowietrzonych i wilgotnych, ale nie podmokłych. Młode rośliny doskonale znoszą zacienienie.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Illustration_Fagus_sylvatica0.jpg/800px-Illustration_Fagus_sylvatica0.jpg",
         },
         grab: {
           displayName: "Grab",
-          description: "",
+          description: "Grab to drzewo liściaste, zrzucające liście na zimę, o szerokiej, często miotastej koronie. Najłatwiej je rozpoznać po zawsze gładkiej, szarej korze z jaśniejszymi paskami. Stanowi on jeden z ważniejszych rodzimych gatunków drzew tworzących lasy w Polsce. Jest drzewem rosnącym nawet w silnym zacienieniu.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Illustration_Carpinus_betulus_1.jpg/800px-Illustration_Carpinus_betulus_1.jpg",
         },
         brzoza: {
           displayName: "Brzoza",
-          description: "",
+          description: "Brzoza jest bardzo charakterystycznym drzewem, nie sposób jej nie rozpoznać ze względu na wyjątkową białą korę, a także delikatne i wiotkie gałązki, zwisające i poruszające się przy podmuchach wiatru. Należy do najpopularniejszych i darzonych największą sympatią drzew, powszechnie sadzonych w parkach i ogrodach, przy domach, na placach i osiedlach.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Illustration_Betula_pendula0.jpg/800px-Illustration_Betula_pendula0.jpg",
         },
         kasztanowiec: {
           displayName: "Kasztanowiec",
-          description: "",
+          description: "Jest to duże i okazałe drzewo liściaste o grubym pniu i kopulastej, często bardzo rozłożystej koronie. Zimą łatwo je rozpoznać z daleka dzięki unikalnemu układowi powyginanych konarów, przechodzących stopniowo w długie, zwisające pionowo niczym grube kable, ciemnobrunatne gałęzie.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Illustration_Aesculus_hippocastanum0.jpg/371px-Illustration_Aesculus_hippocastanum0.jpg",
         },
         sosna: {
           displayName: "Sosna",
-          description: "",
+          description: "Sosna limba występuje na obszarze Alp, Tatr i Karpat Wschodnich. Jest to drzewo iglaste, wolno rosnące. Może osiągnąć wiek do 500 lat. W Polsce w stanie dzikim występuje tylko w Tatrach, gdzie rośnie pojedynczo wśród kosodrzewiny. Limba rośnie na skalistych zboczach, chociaż dobrze czuje się także na nizinach na glinianych glebach, dobrze nawodnionych. Lubi miejsca nasłonecznione.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Illustration_Pinus_sylvestris0.jpg/800px-Illustration_Pinus_sylvestris0.jpg",
         },
         jodla: {
           displayName: "Jodła",
-          description: "",
+          description: "Sylwetką jodła przypomina świerka i modrzewia, jednak w odróżnieniu od nich wierzchołek starych jodeł jest na ogół charakterystycznie spłaszczony, tworząc tzw. bocianie gniazdo. Na tle innych drzew iglastych jodła pospolita wyróżnia się wyraźnie jasną, srebrzystopopielatą i stosunkowo gładką korą oraz stojącymi, przypominającymi z daleka świece, szyszkami.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Illustration_Abies_alba0.jpg/392px-Illustration_Abies_alba0.jpg",
         },
         swierk: {
           displayName: "Świerk",
-          description: "",
+          description: "Świerk pospolity jest uznawany za drugie (po jodle) najwyższe rodzime drzewo europejskie - w sprzyjających warunkach może on osiągać nawet ponad 60m wysokości(!), choć normalnie rzadko przekracza 40m. Dobrą cechą rozpoznawczą świerka pospolitego są jego zwisające szyszki, których podługowaty kształt przypomina „cygaro”.",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Illustration_Picea_abies0_clean.jpg/447px-Illustration_Picea_abies0_clean.jpg",
         },
         modrzew: {
           displayName: "Modrzew",
-          description: "",
+          description: "Bardzo wysokie, zrzucające igły na zimę drzewo iglaste - osiąga 50m wysokości. Igły miękkie i delikatne, zebrane na krótkopędach po kilkadziesiąt sztuk w gęste pęczki. Latem są trawiasto-zielone, jesienią przebarwiają się na żółty kolor. W Polsce na stanowiskach naturalnych modrzew europejski występuje niezwykle rzadko i tylko w Tatrach. ",
           image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Illustration_Larix_decudua0.jpg/448px-Illustration_Larix_decudua0.jpg",
         }
       }
